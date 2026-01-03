8e5732501fcdd88b3e991244d49ad05c6f6f7f00
guilhermesouzadossantos <guihlermesouzasantos@gmail.com>
2026-01-02 21:26:50 -0300

    Create main.py
    Implementação inicial da leitura dos dados do cliente

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+# Leitura da planilha Dados_cliente1 (FATURA)
+df_fatura = pd.read_excel('/Users/guihl/Documents/Workspace/Solar-Panel-Sizing-Tool/data/raw/Dados_cliente1.xlsx')
+
+# Leitura da planilha Dados_cliente1 (INFORMACOES)
+df_informacoes = pd.read_excel('/Users/guihl/Documents/Workspace/Solar-Panel-Sizing-Tool/data/raw/Dados_cliente1.xlsx', sheet_name=1)
+
+# Leitura da planilha HSP-RS
+df_hsp = pd.read_excel('/Users/guihl/Documents/Workspace/Solar-Panel-Sizing-Tool/data/raw/HSP-RS.xlsx')
+
+
+
+# Calcular a media de consumo do cliente
+media_consumo = df_fatura['CONSUMO (KWH)'].mean()
+
+# Definir a potencia do painel escolhido pelo cliente
+potencia_painel = df_informacoes['PAINEL (W)'].max()
