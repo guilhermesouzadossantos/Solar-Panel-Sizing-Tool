232f0a7b6f81064ae5e8b3861c4f0873ee9d3e3e
guilhermesouzadossantos <guihlermesouzasantos@gmail.com>
2026-01-05 21:12:10 -0300

    Cálculo da capacidade do sistema

--- src/main.py
+++ src/main.py
@@ -1,18 +1,15 @@
 import pandas as pd
-
-# LEITURA DE DADOS
+import math
 
 # Leitura da planilha Dados_cliente1 (FATURA)
-df_fatura = pd.read_excel('./data/raw/Dados_cliente1.xlsx')
+df_fatura = pd.read_excel('./data/raw/Dados_cliente.xlsx')
 
 # Leitura da planilha Dados_cliente1 (INFORMACOES)
-df_informacoes = pd.read_excel('./data/raw/Dados_cliente1.xlsx', sheet_name=1)
+df_informacoes = pd.read_excel('./data/raw/Dados_cliente.xlsx', sheet_name=1)
 
 # Leitura da planilha HSP-RS
 df_hsp = pd.read_excel('./data/raw/HSP-RS.xlsx')
 
-
-# CALCULAR MÉDIA APARADA COM BASE EM UMA NOVA COLUNA
 
 # Cria nova coluna para remover os valores máximo e mínimo
 df_fatura['CONSUMO (KWH) APARADO'] = df_fatura['CONSUMO (KWH)']
@@ -26,4 +23,18 @@ min_index = df_fatura['CONSUMO (KWH) APARADO'].idxmax()
 df_fatura.loc[min_index, 'CONSUMO (KWH) APARADO'] = 0
 
 # Calcula média aparada desconsiderando linhas com consumo = 0 
-print(df_fatura[df_fatura['CONSUMO (KWH) APARADO'] != 0] ['CONSUMO (KWH) APARADO'].mean())
+consumo_medio = df_fatura[df_fatura['CONSUMO (KWH) APARADO'] != 0] ['CONSUMO (KWH) APARADO'].mean()
+print('Consumo médio mensal:',consumo_medio,'kWh')
+
+
+# Calcula media de consumo diária 
+consumo_medio_diario = consumo_medio / 30
+
+# Define o valor de horas de sol pleno comparando a lista de horas do RS com a cidade do cliente
+hsp = df_hsp.loc[df_hsp['CIDADE'].isin(df_informacoes['CIDADE']), 'HSP'].item()
+
+# Fórmula para calcular a capacidade do sistema (20% de perda)
+capacidade_sistema_kwp = consumo_medio_diario / (hsp * 0.8)
+
+print('Horas de sol pleno:',hsp)
+print(f'Capacidade do sistema: {capacidade_sistema_kwp:.2f} kWp')
